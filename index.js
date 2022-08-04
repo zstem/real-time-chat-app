4ef6fdc02400389d213f0c88b42ca6e2b6211b34
Zack <zackstempowicz@gmail.com>
2022-08-04 13:08:32 -0400

    socketio fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
     credentials: true,
   }));
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
