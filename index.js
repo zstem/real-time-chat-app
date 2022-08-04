b428cfa35254ac7899e607b8d08c7dcb1abceb5e
Zack <zackstempowicz@gmail.com>
2022-08-04 12:52:02 -0400

    socketio fix

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ const server = require('http').Server(app);
 //     credentials: true,
 //   }));
 
-app.all('/', function(req, res, next) {
+app.use('/', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
+
+app.use((req, res) => res.sendFile('/index.html', { root: __dirname }))
 
 
 const io = require('socket.io')(server);
